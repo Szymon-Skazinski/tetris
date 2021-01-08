c1316533ad475ebff273f5afe7408305a7beed3a
Szymon Skaziński <63155273+Szymon-Skazinski@users.noreply.github.com>
2021-01-08 16:44:01 +0100

    Change implementation of JarFileTest class Add dataProviderTest to check if @DataProvider manifestForCheckingJar doesn't throws exception; in case of exception, test failures and target test is skipped

--- src/test/java/com/epam/prejap/tetris/JarFileTest.java
+++ src/test/java/com/epam/prejap/tetris/JarFileTest.java
@@ -4,56 +4,49 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.fail;
 
 @Test(groups = "JarFile")
 public class JarFileTest {
 
-    private static final String jarfilePath = ".\\target\\tetris-0.1.jar";
+    private static final Path jarfilePath = Path.of(".", "target", "tetris.jar");
     private static final String expectedMainClassName = Tetris.class.getCanonicalName();
 
-    @Test(dataProvider = "manifestForCheckingJar")
+    @Test(dataProvider = "manifestForCheckingJar", dependsOnMethods = "dataProviderTest")
     public void shallContainsSameMainClassName(Manifest manifest) {
-        if (manifest != null) {
-            //given
-            Attributes mainAttributes = manifest.getMainAttributes();
+        //given
+        Attributes mainAttributes = manifest.getMainAttributes();
 
-            //when
-            String actualJarMainClassName = mainAttributes.getValue("Main-Class");
+        //when
+        String actualJarMainClassName = mainAttributes.getValue("Main-Class");
 
-            //then
-            assertEquals(actualJarMainClassName, expectedMainClassName,
-                    String.format("Manifest file shall contains %s as main class name, but did not",
-                            expectedMainClassName));
-        }
-        assertNotNull(manifest, "Manifest file should not be null");
+        //then
+        assertEquals(actualJarMainClassName, expectedMainClassName,
+                String.format("Manifest file shall contains %s as main class name, but did not",
+                        expectedMainClassName));
     }
 
     @DataProvider
     public static Object[][] manifestForCheckingJar() {
-        JarFile jarFile = null;
         Manifest manifest = null;
-
         try {
-            jarFile = new JarFile(jarfilePath);
+            JarFile jarFile = new JarFile(jarfilePath.toString());
+            manifest = jarFile.getManifest();
         } catch (IOException e) {
-            System.err.println("Jar file " + e.getMessage() + " doesn't exist");
+            fail(".jar or MANIFEST.MF file does not exist");
         }
-
-        if (jarFile != null) {
-            try {
-                manifest = jarFile.getManifest();
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
-        }
-
         return new Object[][]{{manifest}};
+    }
+
+    @Test
+    public void dataProviderTest() {
+        manifestForCheckingJar();
     }
 
 }
